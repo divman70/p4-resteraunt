b734dff862cf504800f2eff6076ceda93f26667c
divman70 <dkoch@eagles.ewu.edu>
2015-02-22 21:55:14 -0800

    added js & imgs

---
+++ js/scripts.js
@@ -0,0 +1,81 @@
+// Suhi Menu Items
+
+// Sushi Menu item #1
+$('.first').hover(
+    function(){
+      $(this).attr('src','img/sushiPlate.jpg')
+    },
+    function(){
+      $(this).attr('src','img/Spiral Ecstasy Roll.png')
+    }
+);
+
+// Sushi Menu item #2
+$('.second').hover(
+    function(){
+      $(this).attr('src','img/sushiPlate.jpg')
+    },
+    function(){
+      $(this).attr('src','img/Big Tempura Maki.png')
+    }
+); 
+
+// Sushi Menu item #3
+$('.third').hover(
+    function(){
+      $(this).attr('src','img/sushiPlate.jpg')
+    },
+    function(){
+      $(this).attr('src','img/Kanki By The Sea.jpg')
+    }
+); 
+
+// Sushi Menu item #4
+$('.fourth').hover(
+    function(){
+      $(this).attr('src','img/sushiPlate.jpg')
+    },
+    function(){
+      $(this).attr('src','img/Banzai Roll.jpg')
+    }
+); 
+
+// Sushi Menu item #5
+$('.fifth').hover(
+    function(){
+      $(this).attr('src','img/sushiPlate.jpg')
+    },
+    function(){
+      $(this).attr('src','img/Scallop Banzai Roll.png')
+    }
+); 
+
+// Sushi Menu item #6
+$('.sixth').hover(
+    function(){
+      $(this).attr('src','img/sushiPlate.jpg')
+    },
+    function(){
+      $(this).attr('src','img/Tuna Rio.png')
+    }
+); 
+
+// Sushi Menu item #7
+$('.seventh').hover(
+    function(){
+      $(this).attr('src','img/sushiPlate.jpg')
+    },
+    function(){
+      $(this).attr('src','img/Sheila Maki.png')
+    }
+); 
+
+// Sushi Menu item #8
+$('.eighth').hover(
+    function(){
+      $(this).attr('src','img/sushiPlate.jpg')
+    },
+    function(){
+      $(this).attr('src','img/Volcano Roll.png')
+    }
+); 
